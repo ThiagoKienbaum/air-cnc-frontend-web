f4e311aa06472cc7062d8c337b43e83c695faa04
ThiagoKienbaum <thiago.kienbaum@hotmail.com>
2019-10-09 10:50:24 -0300

    0.5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './assets/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="container">
+      <img src={logo} alt="AirCnC" />
+
+      <div className="content">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <form >
+          <label htmlFor="email">E-mail *</label>
+          <input 
+            type="email"
+            id="email"
+            placeholder="Seu melhor e-mail" 
+            />
+            <button type="submit" >Entrar</button>
+        </form>
+      </div>
+    </div>    
   );
 }
 
